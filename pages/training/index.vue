5f4e254a53bcc648c3c99caa28e4d165dc47208f
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2020-07-28 01:37:02 +0530

    cards

--- pages/training/index.vue
+++ pages/training/index.vue
@@ -1,11 +1,62 @@
 <template>
-<div>Training</div>    
+    <div>
+        <br> <br>
+        <v-container class="grey lighten-5">
+    <v-row no-gutters>
+      <v-col
+        v-for="n in card"
+        :key="n"
+        cols="12"
+        sm="4"
+      >
+        <course 
+            :title="n.title"
+            :body="n.body"
+            :link="n.link"
+            :image="n.image"/>
+      </v-col>
+    </v-row>
+  </v-container>
+    </div>
 </template>
 
 
 <script>
+import cardVue from '../../components/card.vue'
 export default {
-    
+    components: {
+        course: cardVue
+    },
+    data(){
+        return{
+            card: [
+                {
+                    title: 'Python',
+                    body: 'Python',
+                    link: '/training/python',
+                    image: 'python.jpg'
+                },
+                {
+                    title: 'Python',
+                    body: 'Python',
+                    link: '/training/python',
+                    image: 'python.jpg'
+                },
+                {
+                    title: 'Python',
+                    body: 'Python',
+                    link: '/training/python',
+                    image: 'python.jpg'
+                },
+                {
+                    title: 'Python',
+                    body: 'Python',
+                    link: '/training/python',
+                    image: 'python.jpg'
+                }
+            ]
+        };
+    }
 }
 </script>
 
