5b2d4d84461446b1cbb4b9f73e4ab6d24d71e3b9
codetheorem <hrishikeshagarwalv@gmail.com>
2020-07-28 23:14:02 +0530

    added mobile suuport for navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -5,16 +5,36 @@
       dense
       dark
     >
-
+    <v-app-bar-nav-icon @click="drawer = true" v-if="screensize"></v-app-bar-nav-icon>
     <nuxt-link to="/" class="home"><v-toolbar-title>Sudan's Tech</v-toolbar-title></nuxt-link>
 
       <v-spacer></v-spacer>
-      <div class="navbtn">
+      <div class="navbtn" v-if="!screensize">
       <v-btn v-for="(item,i) in items" :key="i" :to="item.to">
           {{item.title}}
       </v-btn>
       </div>
     </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+      v-if="screensize"
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item v-for="(item,i) in items" :key="i" :to="item.to">
+            <v-list-item-title>{{item.title}}</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
   </div>
 </template>
 
@@ -22,6 +42,22 @@
 export default {
 name: "navbar",
 props:['items'],
+data: function (){
+  return{
+    drawer: false,
+  }
+},
+computed:{
+  screensize: function(){
+    switch (this.$vuetify.breakpoint.name) {
+      case 'xs': return true
+      case 'sm': return true
+      case 'md': return false
+      case 'lg': return false
+      case 'xl': return false
+  }
+  }
+}
 }
 </script>
 
--- store/events.js
+++ store/events.js
@@ -1,13 +1,13 @@
 export const state = () => ({
-    upcomingevents: [{ heading: 'No Upcoming Events',
-                        description: null,
-                        link: '#'
-                     }],
-    ongoingevents: [{
-       heading: 'Hack the Mountains',
-       description: 'International Hackathon',
-       link: 'https://www.hackthemountain.tech/'
-    }],
+    upcomingevents: [{
+                        heading: 'Hack the Mountains',
+                        description: 'International Hackathon',
+                        link: 'https://www.hackthemountain.tech/'},
+                    ],
+    ongoingevents: [{ heading: 'No Ongoing Events',
+                      description: null,
+                      link: '#'
+                    },],
     pastevents: [{
         name: 'No Past events',
         date: 'NA',
