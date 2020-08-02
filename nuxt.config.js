d471f7004dbb07a8dfc52ce500cb56f18528a6fe
Hrishikesh Agarwal <hrishikeshagarwalv@gmail.com>
2020-08-02 12:36:24 +0530

    home update (#24)

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
+    titleTemplate: process.env.npm_package_name,
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
@@ -24,7 +24,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/logo.png' },
       {rel:"stylesheet",href:"https://use.fontawesome.com/releases/v5.0.13/css/all.css"}
     ]
   },
