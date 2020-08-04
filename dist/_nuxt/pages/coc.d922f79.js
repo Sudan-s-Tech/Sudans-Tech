ed564d460b59a8836b283fe2fd35c2e7eef7cd83
Vinayak Sharma <vinayaksh42@gmail.com>
2020-08-04 19:12:55 +0530

    dist

--- dist/_nuxt/pages/coc.d922f79.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{419:function(t,n,e){"use strict";e.r(n);var c={},o=e(42),component=Object(o.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("contact")])}),[],!1,null,"15bda02b",null);n.default=component.exports}}]);
