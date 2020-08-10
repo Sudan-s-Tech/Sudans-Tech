a8f51006b8c4b7c3b3931e16b3a83ec50387c398
codetheorem <hrishikeshagarwalv@gmail.com>
2020-08-10 18:59:44 +0530

    event-api

--- components/eventcardgrid.vue
+++ components/eventcardgrid.vue
@@ -17,6 +17,7 @@
 
 <script>
 import eventcard from "@/components/eventcard"
+import axios from 'axios'
 export default {
     name: 'eventcardgrid',
     components:{
@@ -30,10 +31,16 @@ export default {
         }    
       },
     mounted(){
+      axios.get('https://sudans-tech.firebaseio.com/events.json')
+       .then(res=>{
+         this.$store.state.events.events=res.data
+       })
+      console.log(this.$store.state.events.events)
       var eventitems=this.$store.state.events.events
       this.getNow()
       if(this.type==='upcomingevents'){
-        for(var i=0;i<eventitems.length;i++){
+        for( let i in eventitems){
+          console.log(eventitems[i])
           if(parseInt(eventitems[i].startdate.split('-')[2])>parseInt(this.currentdate.split('-')[2])){
             this.items.push(eventitems[i])
           }
@@ -58,7 +65,7 @@ export default {
               })
       }
       if(this.type==='ongoingevents'){
-        for(var i=0;i<eventitems.length;i++){
+        for(let i in eventitems){
           if(parseInt(eventitems[i].enddate.split('-')[2])>parseInt(this.currentdate.split('-')[2]) && parseInt(eventitems[i].startdate.split('-')[2])<parseInt(this.currentdate.split('-')[2])){
             this.items.push(eventitems[i])
           }
